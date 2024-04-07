11eb27c21edd3840ac5d9b0f6802a76736149953
Thato Mongwe <thatomongwe47@gmail.com>
2024-04-07 15:43:46 +0200

    Update shell.h

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define PROMPT "#cisfun$ "
+
+void execute_command(char *command);
+
+#endif

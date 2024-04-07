b08f646eb09609390926234e0357cf335449764b
Thato Mongwe <thatomongwe47@gmail.com>
2024-04-07 15:45:07 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,6 +1,11 @@
 #include "shell.h"
 
-void execute_command(char *command) {
+/**
+ * execute_command - executes a command
+ * @command: the command to execute
+ */
+void execute_command(char *command)
+{
     char *args[2];
     pid_t child_pid;
     int stat_loc;
@@ -9,25 +14,39 @@ void execute_command(char *command) {
     args[1] = NULL;
 
     child_pid = fork();
-    if (child_pid == -1) {
+    if (child_pid == -1)
+    {
         perror("Fork failed");
         exit(1);
-    } else if (child_pid == 0) {
-        if (execve(args[0], args, NULL) == -1) {
+    }
+    else if (child_pid == 0)
+    {
+        if (execve(args[0], args, NULL) == -1)
+        {
             perror("Command not found");
             exit(1);
         }
-    } else {
+    }
+    else
+    {
         waitpid(child_pid, &stat_loc, 0);
     }
 }
 
-int main() {
+/**
+ * main - the main function of the shell
+ *
+ * Return: 0 on success, 1 on failure
+ */
+int main(void)
+{
     char line[256];
 
-    while (1) {
+    while (1)
+    {
         printf("%s", PROMPT);
-        if (fgets(line, sizeof(line), stdin) == NULL) {
+        if (fgets(line, sizeof(line), stdin) == NULL)
+        {
             printf("\n");
             break;
         }
